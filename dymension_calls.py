ceaace5dea6c09b9105a9b43c3a4558dcf04d212
Itzhak Bokris <itzhakbokris@Itzhaks-MacBook.local>
2023-07-04 00:09:19 +0300

    Support multiple environments

--- dymension_calls.py
+++ dymension_calls.py
@@ -5,30 +5,17 @@ dymd utility functions
 - node status
 - tx bank send
 """
-
 import json
 import subprocess
 import logging
-import toml
-import sys
 
-# Load config
-config = toml.load('config.toml')
-
-try:
-    HUB_RPC = config['hub_rpc']
-    HUB_CHAIN_ID = config['hub_chain_id']
-    HUB_EXECUTABLE = config['hub_executable']
-    HUB_DENOM = config['hub_denom']
-except KeyError as key:
-    logging.critical('Key could not be found: %s', key)
-    sys.exit()
+from faucet_types import FaucetEnv
 
 
-def execute(params, chain_id=True, json_output=True):
-    params = [HUB_EXECUTABLE] + params + [f"--node={HUB_RPC}"]
+def execute(env: FaucetEnv, params, chain_id=True, json_output=True):
+    params = [env.node_executable] + params + [f"--node={env.node_rpc}"]
     if chain_id:
-        params.append(f"--chain-id={HUB_CHAIN_ID}")
+        params.append(f"--chain-id={env.node_chain_id}")
     if json_output:
         params.append('--output=json')
     result = subprocess.run(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -41,12 +28,12 @@ def execute(params, chain_id=True, json_output=True):
         raise cpe
 
 
-def check_address(address: str):
+def check_address(env: FaucetEnv, address: str):
     """
     dymd keys parse <address>
     """
     check = subprocess.run(
-        [HUB_EXECUTABLE, "keys", "parse", f"{address}", '--output=json'],
+        [env.node_executable, "keys", "parse", f"{address}", '--output=json'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True)
@@ -62,55 +49,62 @@ def check_address(address: str):
         raise index_error
 
 
-def fetch_network_and_denom_from_trace(denom_trace, original_denom=False):
+def fetch_denom_from_trace(env: FaucetEnv, denom_trace, original_denom=False):
     path = denom_trace["path"]
     base_denom = denom_trace["base_denom"]
     path_parts = str(path).split("/")
-    client_state = execute(["query", "ibc", "channel", "client-state", "transfer", path_parts[len(path_parts) - 1]])
+    client_state = \
+        execute(env, ["query", "ibc", "channel", "client-state", "transfer", path_parts[len(path_parts) - 1]])
     result = {"network_id": client_state["client_state"]["chain_id"], "denom": base_denom}
 
     if original_denom:
-        denom_hash = execute(["query", "ibc-transfer", "denom-hash", f'{path}/{base_denom}'])
+        denom_hash = execute(env, ["query", "ibc-transfer", "denom-hash", f'{path}/{base_denom}'])
         result['original_denom'] = f'ibc/{denom_hash["hash"]}'
 
     return result
 
 
-def fetch_network_denom_list(original_denom=False):
-    response = execute(["query", "ibc-transfer", "denom-traces"])
+def fetch_network_denom_list(env: FaucetEnv, original_denom=False):
+    response = execute(env, ["query", "ibc-transfer", "denom-traces"])
     network_denom_list = list(map(
-        lambda trace: fetch_network_and_denom_from_trace(trace, original_denom), response['denom_traces']))
-    network_denom_list.append({"network_id": HUB_CHAIN_ID, "denom": HUB_DENOM})
-    return network_denom_list
+        lambda trace: fetch_denom_from_trace(env, trace, original_denom), response['denom_traces']))
+    fixed_list = [{"network_id": env.node_chain_id, "denom": env.node_denom}]
+
+    for network_denom in network_denom_list:
+        exist_denom = next((item for item in fixed_list if item["denom"] == network_denom['denom']), None)
+        if not exist_denom:
+            fixed_list.append(network_denom)
+
+    return fixed_list
 
 
-def get_fixed_balance_denom(balance):
+def get_fixed_balance_denom(env: FaucetEnv, balance):
     denom = balance["denom"]
     if denom.startswith('ibc/'):
-        response = execute(["query", "ibc-transfer", "denom-trace", denom])
+        response = execute(env, ["query", "ibc-transfer", "denom-trace", denom])
         balance['denom'] = response['denom_trace']['base_denom']
     balance['amount'] = float(balance['amount'])
     return balance
 
 
-def get_balances(address: str):
+def get_balances(env: FaucetEnv, address: str):
     """
     dymd query bank balances <address> <node> <chain-id>
     """
     try:
-        response = execute(["query", "bank", "balances", address])
+        response = execute(env, ["query", "bank", "balances", address])
         balances = response['balances']
-        return list(map(get_fixed_balance_denom, balances))
+        return list(map(lambda balance: get_fixed_balance_denom(env, balance), balances))
     except IndexError as index_error:
         logging.error('Parsing error on balance request: %s', index_error)
         raise index_error
 
 
-def get_node_status():
+def get_node_status(env: FaucetEnv):
     """
     dymd status <node>
     """
-    status = execute(["status"], chain_id=False, json_output=False)
+    status = execute(env, ["status"], chain_id=False, json_output=False)
     try:
         node_status = {
             'moniker': status['NodeInfo']['moniker'],
@@ -124,11 +118,11 @@ def get_node_status():
         raise key
 
 
-def get_tx_info(hash_id: str):
+def get_tx_info(env: FaucetEnv, hash_id: str):
     """
     dymd query tx <tx-hash> <node> <chain-id>
     """
-    tx_response = execute(['query', 'tx', f'{hash_id}'])
+    tx_response = execute(env, ['query', 'tx', f'{hash_id}'])
     try:
         tx_body = tx_response['tx']['body']['messages'][0]
         tx_out = {'height': tx_response['height']}
@@ -150,18 +144,18 @@ def get_tx_info(hash_id: str):
         raise KeyError from err
 
 
-def tx_send(sender: str, recipient: str, amount: str, fees: int):
+def tx_send(env: FaucetEnv, sender: str, recipient: str, amount: str, fees: int):
     """
     dymd tx bank send <from address> <to address> <amount> <fees> <node> <chain-id> --keyring-backend=test -y
     """
-    response = execute([
+    response = execute(env, [
         'tx',
         'bank',
         'send',
         sender,
         recipient,
         amount,
-        f'--fees={fees}{HUB_DENOM}',
+        f'--fees={fees}{env.node_denom}',
         '--keyring-backend=test',
         '-y'
     ])
